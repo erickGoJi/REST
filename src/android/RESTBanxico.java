6fdeb38be6c8b129e23c1539203aa9b09e6d27c7
erick gomez <erickgomezlol96@gmail.com>
2019-06-30 10:44:29 -0500

    Modifies

--- src/android/RESTBanxico.java
+++ src/android/RESTBanxico.java
@@ -1,4 +1,4 @@
-package cordova-plugin-rest-banxico;
+package cordova.plugin.rest.banxico.RESTBanxico;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
--- www/RESTBanxico.js
+++ www/RESTBanxico.js
@@ -1,5 +1,10 @@
 var exec = require('cordova/exec');
+var PLUGIN_NAME = 'RESTBanxico';
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'RESTBanxico', 'coolMethod', [arg0]);
+var RESTBanxico = {
+    coolMethod = function (arg0, success, error) {
+        exec(success, error, 'RESTBanxico', 'coolMethod', [arg0])
+    }
 };
+
+module.exports = RESTBanxico;
