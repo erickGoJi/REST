fe4bb59335e45ed5506055219977083ff185649f
Minimalist <erick@minimalist.mx>
2019-07-01 16:43:55 -0500

    Add Post funtion

--- src/android/RESTBanxico.java
+++ src/android/RESTBanxico.java
@@ -7,6 +7,19 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This class echoes a string called from JavaScript.
  */
@@ -18,6 +31,9 @@ public class RESTBanxico extends CordovaPlugin {
             String message = args.getString(0);
             this.coolMethod(message, callbackContext);
             return true;
+        } else if (action.equals("sendPost")){
+            this.postVolley(args.getString(0), args.getString(1l), callbackContext);
+            return true;
         }
         return false;
     }
@@ -28,5 +44,50 @@ public class RESTBanxico extends CordovaPlugin {
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+
+    private void postVolley(String endPoint,String requestBody,CallbackContext callbackContext){
+        Context context = this.cordova.getActivity().getApplicationContext();
+        RequestQueue MyRequestQueue = Volley.newRequestQueue(context);
+        final String API_URL = "https://www.banxico.org.mx/"+endPoint;
+        //final String requestBody = "d= {'nc': '5551359763','idH': '0cdbf9419ccf46c5','ia': {'so': 'Android','vSO': '9','fab': 'moto','mod': 'moto g(6) plus'}}";
+        StringRequest MyStringRequest = new StringRequest(Request.Method.POST, API_URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //This code is executed if the server responds, whether or not the response contains data.
+                //The String 'response' contains the server's response.
+                Log.i("Response ",response);
+                callbackContext.success(response);
+            }
+        }, new Response.ErrorListener() { //Create an error listener to handle errors appropriately.
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                //This code is executed if there is an error.
+                callbackContext.error(response);
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "text/plain");
+                return params;
+            }
+
+            @Override
+            public String getBodyContentType() {
+                return "text/plain; charset=utf-8";
+            }
+
+            @Override
+            public byte[] getBody() throws AuthFailureError {
+                try {
+                    return requestBody == null ? null : requestBody.getBytes("utf-8");
+                } catch (UnsupportedEncodingException uee) {
+                    VolleyLog.wtf("Unsupported Encoding while trying to get the bytes of %s using %s", requestBody, "utf-8");
+                    return null;
+                }
+            }
+        };
+        MyRequestQueue.add(MyStringRequest);
     }
 }
--- www/RESTBanxico.js
+++ www/RESTBanxico.js
@@ -4,6 +4,9 @@ var PLUGIN_NAME = 'RESTBanxico';
 var RESTBanxico = {
     coolMethod: function (arg0, success, error) {
         exec(success, error, 'RESTBanxico', 'coolMethod', [arg0])
+    },
+    sendPost: function(endPoint,requestbody){
+        exec(success,error,'sendPost',[endPoint,requestbody]);   
     }
 };
 
