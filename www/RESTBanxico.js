457d26f71ce3b1a8d809773c14ace9c3d58ae493
Minimalist <erick@minimalist.mx>
2019-07-01 19:39:12 -0500

    Modified

--- www/RESTBanxico.js
+++ www/RESTBanxico.js
@@ -5,8 +5,8 @@ var RESTBanxico = {
     coolMethod: function (arg0, success, error) {
         exec(success, error, 'RESTBanxico', 'coolMethod', [arg0])
     },
-    sendPost: function(endPoint,requestbody){
-        exec(success,error,'RESTBanxico','sendPost',[endPoint,requestbody]);   
+    sendPost: function(endPoint,requestbody,success){
+        exec(success,null,'RESTBanxico','sendPost',[endPoint,requestbody]);   
     }
 };
 
